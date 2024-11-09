458121f370a0df893afdce3c2cfae079a8bb7926
yorickburgers <yorickburgers@live.nl>
2024-11-09 14:28:21 +0100

    Opdracht 2 done + styling buttons

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ import tvString from './helpers/tvStringGenerator.js'
 import tvPrice from './helpers/priceStringGenerator.js'
 import {size} from './helpers/sizeStringGenerator.js'
 import {bestSellingTv} from './constants/inventory.js'
+import check from './assets/check.png'
+import minus from './assets/minus.png'
 
 function App() {
   return (
-    <>
-      <h1>Tech it easy dashboard</h1>
-      <h3>Verkoopoverzicht</h3>
-      <span className="categoryWrapper">
+      <>
+          <h1>Tech it easy dashboard</h1>
+          <h3>Verkoopoverzicht</h3>
+          <span className="categoryWrapper">
         <div className="category" id="sold">
           <p>Aantal verkochte producten</p>
           <p>{soldTV()}</p>
@@ -26,18 +28,28 @@ function App() {
           <p>{toSell()}</p>
         </div>
       </span>
-      <h3>Best verkochte tv</h3>
-        <div className="categoryWrapper">
-            <div className="category" id="image">
-                <img src={bestSellingTv.sourceImg} alt="image of bestelling tv" />
-            </div>
-            <div className="category" id="bestSelling">
-                <p>{tvString()}</p>
-                <p>{tvPrice()}</p>
-                <p>{size()}</p>
-            </div>
-        </div>
-    </>
+          <h3>Best verkochte tv</h3>
+          <div className="categoryWrapper" id={"bestSellingTV"}>
+              <div className="category" id="image">
+                  <img src={bestSellingTv.sourceImg} alt="image of bestelling tv"/>
+              </div>
+              <div className="category" id="bestSelling">
+                  <p>{tvString()}</p>
+                  <h3>{tvPrice()}</h3>
+                  <p>{size()}</p>
+                  <p>
+                      <img src={check} alt="Check icon" style={{width: '20px', height: '20px'}}/> wifi
+                      <img src={minus} alt="Minus icon" style={{width: '20px', height: '20px'}}/> speech
+                      <img src={check} alt="Check icon" style={{width: '20px', height: '20px'}}/> hdr
+                      <img src={check} alt="Check icon" style={{width: '20px', height: '20px'}}/> bluetooth
+                      <img src={minus} alt="Minus icon" style={{width: '20px', height: '20px'}}/> ambilight
+                  </p>
+              </div>
+          </div>
+          <button type="button">Meest verkocht eerst</button>
+          <button type="button">Goedkoopste eerst</button>
+          <button type="button">Meest geschikt voor sport eerst</button>
+      </>
   )
 }
 
